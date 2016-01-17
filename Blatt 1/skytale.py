2412b4b12b0cfeb5a8a434b9e170efcd757ab345
lgoerke <lgoerke@uni-osnabrueck.de>
2016-01-17 18:41:11 +0100

    Revert "yoo"
    This reverts commit 41827019b5976aa6a627921a36187c80ab36b66d.

--- Blatt 1/skytale.py
+++
@@ -1,8 +0,0 @@
-cryptotext = 'PLTIRIERTFEIAIASSNOHKTSNRYEPSETIOVNANSE'
-k = 5; w = len(cryptotext)
-cleartext = ''.join(
-    [''.join(
-            [cryptotext[i] for i in range(j, w, k)]) 
-        for j in range(0, k)]
-)
-print(cleartext)
